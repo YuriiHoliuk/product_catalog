4258169d4b79647ba4d7a5c63e904947f3ae9036
YuriiHoliuk <yurii.holiuk@mate.academy>
2022-11-02 22:57:14 +0200

    Procfile

--- server/dist/index.js
+++ server/dist/index.js
@@ -1,9 +1,12 @@
 import express from "express";
+
 var PORT = 3000;
 var app = express();
+
 app.get("/test", function(req, res) {
     res.status(200).send("Hello World");
 });
+
 app.listen(PORT, function() {
     console.log("Server running on port ".concat(PORT));
 });
